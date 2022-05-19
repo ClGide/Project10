e9424ac6828baf5bc4bcc1b776f4c362ee431c93
ClGide <90548791+ClGide@users.noreply.github.com>
2022-05-19 11:39:32 +0200

    Update settings.py

--- softdesk/settings.py
+++ softdesk/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-a=+i!#o6x38jsyp5neji7%pfl*69eo%2*^z#g2k6c)ou4!_@-u'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
